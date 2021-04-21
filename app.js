cffec932c0fcab332b8f447a932f86b50546bcaa
Rabin Gaire <rabingaire20@gmail.com>
2021-04-21 18:05:24 +0545

    fix cors error

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 // For local development
