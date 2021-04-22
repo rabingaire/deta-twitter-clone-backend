f66c1f5dac45bb353ce4a782dfd7493e48923528
Rabin Gaire <rabingaire20@gmail.com>
2021-04-22 22:03:30 +0545

    add new origin on cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://6xnhbj.deta.dev"],
   })
 );
 app.use(express.json());
